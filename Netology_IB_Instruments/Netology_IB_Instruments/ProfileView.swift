bf73958f6db7b3d7e4bd77515b51ad50b3481a6c
Mastepan Evgeni <mail@mastepan.ru>
2022-02-21 10:23:49 +0700

    Сделано по букве задания, но ещё не работает. Ибо...

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Evgeny Mastepan on 21.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var UserResumeUIImageView: UIImageView!
+
+    @IBOutlet weak var userResumeUILabel: UILabel!
+
+    @IBOutlet weak var userResumeBirthdayUILabel: UILabel!
+
+    @IBOutlet weak var userResumeLivePlaceUILabel: UILabel!
+
+    @IBOutlet weak var userResumeSummaryUITextView: UITextView!
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
