17528dd0ef649fefdb9bef107cc3ff45336ebfcc
Mastepan Evgeni <mail@mastepan.ru>
2022-03-03 21:53:42 +0700

    Task 6 of 1.3.1 completed.

---
+++ Navigation/Navigation/PostViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Evgeny Mastepan on 03.03.2022.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemCyan
+        self.navigationItem.title = "Current Post"
+
+ 
+    }
+
+}
