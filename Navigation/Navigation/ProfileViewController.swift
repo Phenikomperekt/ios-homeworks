a436e80d282e69a4e80cfc155850506ba255de53
Mastepan Evgeni <mail@mastepan.ru>
2022-03-15 16:41:11 +0700

    Task 2.1

--- Navigation/Navigation/ProfileViewController.swift
+++ Navigation/Navigation/ProfileViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+
+
+    private lazy var profileHeaderView: ProfileHeaderView = {
+        let profileHeaderView = ProfileHeaderView()
+
+
+
+        return profileHeaderView
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemGray2
+        self.view.addSubview(profileHeaderView)
+        profileHeaderView.addSubViews()
+
     }
+
+
+    override func viewWillLayoutSubviews() {
+        profileHeaderView.frame.size = self.view.frame.size
+    }
+
 
 }
